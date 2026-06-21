bf53febd3f133fd02c599df093f5e59aed42ca3b
GabrielSilva66 <gabriel.victor.silva.706@ufrn.edu.br>
2026-06-21 10:12:57 -0300

    capiva-core initial commit

---
+++ capiva-core/src/main/java/com/example/capiva_core/CapivaCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.capiva_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapivaCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapivaCoreApplication.class, args);
+	}
+
+}
